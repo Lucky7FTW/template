c6d0441bf0df82e8c16cdcf3829e7d1cb88b4ad3
Lucky7 <mihaitibrea2@gmail.com>
2025-01-30 13:58:08 +0200

    refacut navbar cu optiune de dropdown

--- template/src/app/navbar/navbar.component.ts
+++ template/src/app/navbar/navbar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-navbar',
-  standalone: true, 
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @Input() dropdownTrigger: 'hover' | 'click' = 'hover';
+
+  // Track which dropdowns are open
+  isDropdownOpen: { [key: string]: boolean } = {
+    nav1: false,
+    nav2: false,
+    dropdown: false
+  };
+
+  ngOnInit() {}
+
+  toggleDropdown(key: string) {
+    if (this.dropdownTrigger === 'click') {
+      this.isDropdownOpen[key] = !this.isDropdownOpen[key];
+    }
+  }
+
+  onMouseEnter(key: string) {
+    if (this.dropdownTrigger === 'hover') {
+      this.isDropdownOpen[key] = true;
+    }
+  }
+
+  onMouseLeave(key: string) {
+    if (this.dropdownTrigger === 'hover') {
+      this.isDropdownOpen[key] = false;
+    }
+  }
 }
