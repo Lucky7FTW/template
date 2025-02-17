6b7de01f579b13c7f961da0bcddfd075e12d69c9
Lucky7 <mihaitibrea2@gmail.com>
2025-02-17 11:21:03 +0200

    reparat rute

--- template/src/app/app.routes.ts
+++ template/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CascadingDropdownPageComponent } from './components/cascading-data/cascading-dropdown-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 
-  { path: 'cascading', component: CascadingDropdownPageComponent},
+  { path: 'cascading', component: CascadingDropdownPageComponent,canActivate: [AuthGuard]},
 
-  { path: '**', redirectTo: '/' }
+  { path: 'not-found', component: NotFoundComponent },
+
+  { path: '**', redirectTo: '/not-found' }
 ];
